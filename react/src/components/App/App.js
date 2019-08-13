6528609629bdeca98b0e7ee88dd87bb459823c2c
Paulo Sandoval <ez2torta@gmail.com>
2019-08-13 04:46:18 -0400

    Added sagas, materialui and hotreload?

---
+++ react/src/components/App/App.js
@@ -0,0 +1,16 @@
+// App.js
+
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+
+class App extends Component {
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+        Welcome Material UI
+      </Button>
+    );
+  }
+}
+
+export default App;
