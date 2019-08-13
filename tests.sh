20169f22d120475e83ca4989d412ce79e2812b0c
Paulo Sandoval <ez2torta@gmail.com>
2019-08-13 00:15:33 -0400

    First Commit, Docker working?

---
+++ tests.sh
@@ -0,0 +1,1 @@
+docker-compose exec app vendor/bin/phpunit --testdox
---
+++ tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Auth;
+use App\User;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class LoginTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+        $response = $this->actingAs($user)->get('/login');
+        $response->assertRedirect('/home');
+    }
+
+    // public function test_user_can_login_with_correct_credentials()
+    // {
+    //     $user = factory(User::class)->create([
+    //         'password' => bcrypt($password = 'i-love-laravel'),
+    //     ]);
+    //     $response = $this->post('/login', [
+    //         'email' => $user->email,
+    //         'password' => $password,
+    //     ]);
+    //     $response->assertRedirect('/home');
+    //     $this->assertAuthenticatedAs($user);
+    // }
+}
